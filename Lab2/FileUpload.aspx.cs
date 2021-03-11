2ab586a40723f03c6c09037ced71cea7e83bacf6
Alec Thoman <thomanac@dukes.jmu.edu>
2021-03-11 12:23:46 -0500

    added email notif when new customer folder is created

--- Lab2/FileUpload.aspx.cs
+++ Lab2/FileUpload.aspx.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading;
 using System.Web;
 using System.Web.UI;
@@ -25,7 +27,11 @@ namespace Lab2
                 {
                     //If no directory then creates the new one
                     Directory.CreateDirectory(NewDirectory);
-                    //Label1.Text = "Directory Created";
+
+                    string from = "GreenValleyServices2021@gmail.com";
+                    string to = "thomanac@dukes.jmu.edu";
+                    sendEmail(from, to);
+                    
                 }
                 else
                 {
@@ -50,6 +56,7 @@ namespace Lab2
             {
                 FileUpload1.SaveAs(newDirectory + "/" + FileUpload1.FileName);
                 Label2.Text = "Image/File Uploaded Successfully!";
+
             }
             else
             {
@@ -66,6 +73,36 @@ namespace Lab2
         {
             Session.Abandon();
             Response.Redirect("CustomerLoginPage.aspx?loggedout=true");
+        }
+
+        private void sendEmail(string fromEmail, string toEmail)
+        {
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Host = "smtp.gmail.com";
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Port = 587;
+            //string fromEmail = "GreenValleyServices2021@gmail.com"; // created this gmail for this as the sole purpose
+            smtpClient.Credentials = new System.Net.NetworkCredential(fromEmail, "GoDukes2021");
+            smtpClient.EnableSsl = true;
+
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(fromEmail);
+            //mailMessage.To.Add("thomanac@dukes.jmu.edu");
+            mailMessage.To.Add(toEmail);
+            mailMessage.Subject = "New Customer Alert!!";
+            mailMessage.Body = "Please check for new customer file created in the system.";
+            try
+            {
+                smtpClient.Send(mailMessage);
+            }
+            catch (Exception ex)
+            {
+                //Error
+                //Console.WriteLine(ex.Message);
+                Response.Write(ex.Message);
+            }
         }
     }
 }
