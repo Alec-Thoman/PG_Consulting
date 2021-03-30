d387e67553b3010249a2107dc82f8304bd0f9e3c
Yuhui-Li20 <80490066+Yuhui-Li20@users.noreply.github.com>
2021-03-30 13:45:34 -0400

    Add files via upload

---
+++ Lab2/StatusBar.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="StatusBar.aspx.cs" Inherits="Lab2.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <fieldset>
+                <legend>Client Status Bar</legend> 
+                <br />
+                <asp:GridView ID="grdCustomerStatus" runat="server" AlternatingRowStyle-BackColor="#ccffff" EmptyDataText="No Record" ForeColor="Red">
+                </asp:GridView>
+                <asp:Label ID="lblCreate" runat="server" Text="Create/Update Client Status"></asp:Label>
+                <asp:Table ID="Table1" runat="server">
+                    <asp:TableRow>
+                     <asp:TableCell>
+                         <asp:Label ID="lblName" runat="server" Text="Customer Name"></asp:Label>
+                     </asp:TableCell>
+                     <asp:TableCell>
+                         <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                     </asp:TableCell>
+                     </asp:TableRow>
+                    
+                    <asp:TableRow>
+                     <asp:TableCell>
+                         <asp:Label ID="lblStatus" runat="server" Text="Status"></asp:Label>
+                     </asp:TableCell>
+                     <asp:TableCell>
+                         <asp:TextBox ID="txtStatus" runat="server"></asp:TextBox>
+                     </asp:TableCell>
+                     </asp:TableRow>
+                </asp:Table>
+                <asp:Button ID="Button1" runat="server" Text="Create/Update" onclick="Button1_Click"/>
+                </fieldset>
+            <br />
+            <asp:Button ID="Back" runat="server" Text="Back to Navigate Page" onclick="Back_Click"/>
+
+
+</asp:Content>
---
+++ Lab2/StatusBar.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace Lab2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            updateGridView();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String name = txtName.Text;
+            String status = txtStatus.Text;
+            String a = "";
+            SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
+
+            String sqlIDQuery = "Select CustomerName from ClientStatus";
+            SqlCommand sqlCommand1 = new SqlCommand();
+            sqlCommand1.Connection = sqlConnect;
+            sqlCommand1.CommandType = CommandType.Text;
+            sqlCommand1.CommandText = sqlIDQuery;
+
+            sqlConnect.Open();
+
+            SqlDataReader Results = sqlCommand1.ExecuteReader();
+
+            while (Results.Read())
+            {
+                if (name.ToLower() == Results["CustomerName"].ToString().ToLower())
+                {
+                    Results.Close();
+                    String sqlQuery1 = "Update ClientStatus set Status = '"+ status + "' where CustomerName = '" + name +"';";
+
+
+                    SqlCommand sqlCommand2 = new SqlCommand();
+                    sqlCommand2.Connection = sqlConnect;
+                    sqlCommand2.CommandType = CommandType.Text;
+                    sqlCommand2.CommandText = sqlQuery1;
+
+
+                    SqlDataReader queryResults1 = sqlCommand2.ExecuteReader();
+                    queryResults1.Close();
+                    updateGridView();
+                    break;
+                }
+
+                else
+                {
+                    string sqlQuery2 = "Insert into clientStatus(customerName, status) value ('" + name + "','" + status + "')";
+
+                    SqlCommand sqlCommand2 = new SqlCommand();
+                    sqlCommand2.Connection = sqlConnect;
+                    sqlCommand2.CommandType = CommandType.Text;
+                    sqlCommand2.CommandText = sqlQuery2;
+
+                    sqlConnect.Open();
+                    SqlDataReader queryResults = sqlCommand2.ExecuteReader();
+                    queryResults.Close();
+                    sqlConnect.Close();
+
+                }
+           
+
+            
+
+        }
+
+        protected void Back_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void updateGridView()
+        {
+            grdCustomerStatus.DataSource = null;
+            grdCustomerStatus.DataBind();
+
+            String sqlQuery = "SELECT CustomerName as Name_Client, Status from ClientStatus";
+
+            SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
+
+            SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlQuery, sqlConnect);
+
+            DataTable workflowGridview = new DataTable();
+
+            sqlAdapter.Fill(workflowGridview);
+
+            grdCustomerStatus.DataSource = workflowGridview;
+            grdCustomerStatus.DataBind();
+        }
+    }
+}
