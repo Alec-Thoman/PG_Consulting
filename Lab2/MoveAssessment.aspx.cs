c56d3fbce0343f8406756c084ff87c769aeb8c87
Yuhui-Li20 <80490066+Yuhui-Li20@users.noreply.github.com>
2021-04-11 16:24:08 -0400

    Update MoveAssessment.aspx.cs

--- Lab2/MoveAssessment.aspx.cs
+++ Lab2/MoveAssessment.aspx.cs
@@ -1,7 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -12,6 +16,96 @@ namespace Lab2
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //string preliminarySql = "insert into Preliminary
+            //string roomSql = "insert into Room 
+            //string infoSql = "insert into SpecificInfo 
+            //string costSql = "insert into Cost
+             
+            try
+            {
+                using (var connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString))
+                {
+                    connection.Open();
+                    // insert into LookAt Table
+                    using (SqlCommand command = new SqlCommand(preliminarySql, connection))
+                    {
+                        
+                    }
+
+                    // insert into box table
+                    using (SqlCommand cmd = new SqlCommand(roomSql, connection))
+                    {
+                        connection.Open();
+
+                        // checking to make sure tb's are not empty
+                        string empty = "0";
+                        if (TextBox79.Equals(""))
+                        {
+                            cmd.Parameters.Add
+                        else
+                        {
+                            cmd.Parameters.Add
+                        }
+
+                        if (TextBox80.Equals(""))
+                        {
+                            cmd.Parameters.Add
+                        else
+                        {
+                            cmd.Parameters.Add
+
+                        if (TextBox81.Equals(""))
+                        {
+                            cmd.Parameters.Add
+                        }
+                        else
+                        {
+                            cmd.Parameters.Add
+                        }
+
+                        if (TextBox82.Equals(""))
+                        {
+                            cmd.Parameters.Add
+                        else
+                        {
+                            cmd.Parameters.Add
+
+                        if (TextBox83.Equals(""))
+                        {
+                            cmd.Parameters.Add
+                        }
+                        else
+                        {
+                            cmd.Parameters.Add
+
+                        if (TextBox84.Equals(""))
+                        {
+                            cmd.Parameters.Add
+                        }
+                        else
+                        {
+                            cmd.Parameters.Add
+                        }
+
+                        cmd.ExecuteNonQuery();
+                        connection.Close();
+
+                         using (SqlCommand command = new SqlCommand(infoSql, connection))
+                         {
+
+                         }
+
+                        using (SqlCommand command = new SqlCommand(costSql, connection))
+                        {
+
+                        }
+                                            }
+                }
+            }
         }
     }
 }
